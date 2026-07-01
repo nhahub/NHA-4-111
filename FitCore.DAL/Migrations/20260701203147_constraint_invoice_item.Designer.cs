8bb913d31ec2874b717cdc17c7bd07235ea8c10b
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-01 23:32:32 +0300

    change at invoice item constraint

--- FitCore.DAL/Migrations/20260701203147_constraint_invoice_item.Designer.cs
+++ FitCore.DAL/Migrations/20260701203147_constraint_invoice_item.Designer.cs
@@ -4,6 +4,7 @@ using FitCore.DAL.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FitCore.DAL.Migrations
 {
     [DbContext(typeof(FitCoreDbContext))]
-    partial class FitCoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260701203147_constraint_invoice_item")]
+    partial class constraint_invoice_item
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
