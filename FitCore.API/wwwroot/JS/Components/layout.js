6634ed73961289218a17b766cb44c5b939120ba7
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-02 16:47:25 +0300

    audit logs front + back + side bar + header

---
+++ FitCore.API/wwwroot/JS/Components/layout.js
@@ -0,0 +1,16 @@
+﻿// layout.js
+async function loadComponent(elementId, componentPath) {
+    try {
+        const response = await fetch(componentPath);
+        const html = await response.text();
+        document.getElementById(elementId).innerHTML = html;
+    } catch (error) {
+        console.error(`Error loading ${componentPath}:`, error);
+    }
+}
+
+// تحميل المكونات أول ما الصفحة تفتح
+document.addEventListener('DOMContentLoaded', () => {
+    loadComponent('sidebar-container', '/html/Components/sidebar.html');
+    loadComponent('header-container', '/html/Components/header.html');
+});
