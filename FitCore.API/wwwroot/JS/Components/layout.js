aabe40b1310e4ac8676081225b8c5b0a55333772
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-02 18:41:22 +0300

    edits at logs

--- FitCore.API/wwwroot/JS/Components/layout.js
+++ FitCore.API/wwwroot/JS/Components/layout.js
@@ -1,4 +1,4 @@
-﻿// layout.js
+﻿
 async function loadComponent(elementId, componentPath) {
     try {
         const response = await fetch(componentPath);
@@ -8,8 +8,18 @@ async function loadComponent(elementId, componentPath) {
         console.error(`Error loading ${componentPath}:`, error);
     }
 }
+function setActiveLink() {
+    const currentPath = window.location.pathname;
+    const navLinks = document.querySelectorAll('.sidebar-nav a');
 
-// تحميل المكونات أول ما الصفحة تفتح
+    navLinks.forEach(link => {
+        if (link.getAttribute('href') === currentPath) {
+            document.querySelectorAll('.sidebar-nav li').forEach(li => li.classList.remove('active'));
+            link.parentElement.classList.add('active');
+        }
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     loadComponent('sidebar-container', '/html/Components/sidebar.html');
     loadComponent('header-container', '/html/Components/header.html');
