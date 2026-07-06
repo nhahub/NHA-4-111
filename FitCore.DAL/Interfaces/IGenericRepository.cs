f906cfc131863db6cc9188ae2295367ed6c0ac20
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-06 17:03:57 +0300

    removed generic repo and unit of work

--- FitCore.DAL/Interfaces/IGenericRepository.cs
+++ FitCore.DAL/Interfaces/IGenericRepository.cs
@@ -1,28 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Linq.Expressions;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Linq.Expressions;
 
-namespace FitCore.DAL.Interfaces
-{
-    public interface IGenericRepository<T> where T : class
-    {
-        Task<T?> GetByIdAsync(int id);
-        Task<IEnumerable<T>> GetAllAsync();
-        //Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, string[]? includes = null);
+//namespace FitCore.DAL.Interfaces
+//{
+//    public interface IGenericRepository<T> where T : class
+//    {
+//        Task<T?> GetByIdAsync(int id);
+//        Task<IEnumerable<T>> GetAllAsync();
+//        //Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, string[]? includes = null);
 
-        Task AddAsync(T entity);
-        //Task AddRangeAsync(IEnumerable<T> entities);
+//        Task AddAsync(T entity);
+//        //Task AddRangeAsync(IEnumerable<T> entities);
         
-        void Update(T entity);
-        //void UpdateRange(IEnumerable<T> entities);
+//        void Update(T entity);
+//        //void UpdateRange(IEnumerable<T> entities);
         
-        void Delete(T entity);
-        //void DeleteRange(IEnumerable<T> entities);
+//        void Delete(T entity);
+//        //void DeleteRange(IEnumerable<T> entities);
         
-        IQueryable<T> GetAllAsIQueryable();
-        IQueryable<T> GetByIdAsIQueryable(int id);
-    }
-}
+//        IQueryable<T> GetAllAsIQueryable();
+//        IQueryable<T> GetByIdAsIQueryable(int id);
+//    }
+//}
--- FitCore.DAL/Interfaces/IUnitOfWork.cs
+++ FitCore.DAL/Interfaces/IUnitOfWork.cs
@@ -1,19 +1,19 @@
-﻿using FitCore.DAL.Data.Models;
-using Microsoft.EntityFrameworkCore.Storage;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using FitCore.DAL.Data.Models;
+//using Microsoft.EntityFrameworkCore.Storage;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace FitCore.DAL.Interfaces
-{
-    public interface IUnitOfWork : IDisposable
-    {
-        IGenericRepository<T> GetRepository<T>() where T : class;
+//namespace FitCore.DAL.Interfaces
+//{
+//    public interface IUnitOfWork : IDisposable
+//    {
+//        IGenericRepository<T> GetRepository<T>() where T : class;
 
-        Task<int> SaveChangesAsync();
+//        Task<int> SaveChangesAsync();
 
-        Task<IDbContextTransaction> BeginTransactionAsync();
-    }
-}
+//        Task<IDbContextTransaction> BeginTransactionAsync();
+//    }
+//}
