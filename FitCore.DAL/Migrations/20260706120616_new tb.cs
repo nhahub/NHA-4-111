1f377550ad99503a3fb2b1b18d82db63e70a1dd5
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-06 15:06:43 +0300

    new update to db

---
+++ FitCore.DAL/Migrations/20260706120616_new tb.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitCore.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class newtb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GymServiceClass",
+                columns: table => new
+                {
+                    ServiceId = table.Column<int>(type: "int", nullable: false),
+                    ClassId = table.Column<int>(type: "int", nullable: false),
+                    AllowedAttendancesPerMonth = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GymServiceClass", x => new { x.ServiceId, x.ClassId });
+                    table.ForeignKey(
+                        name: "FK_GymServiceClass_Classes_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Classes",
+                        principalColumn: "ClassID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GymServiceClass_GymService_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "GymService",
+                        principalColumn: "ServiceID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GymServiceClass_ClassId",
+                table: "GymServiceClass",
+                column: "ClassId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GymServiceClass");
+        }
+    }
+}
