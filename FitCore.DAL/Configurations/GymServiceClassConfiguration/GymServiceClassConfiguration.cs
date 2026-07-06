21b6ead9975af6e16549a89aa8b13b5e5cc58eef
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-06 14:56:35 +0300

    .

---
+++ FitCore.DAL/Configurations/GymServiceClassConfiguration/GymServiceClassConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using FitCore.DAL.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCore.DAL.Configurations.GymServiceClassConfiguration
+{
+    public class GymServiceClassConfiguration : IEntityTypeConfiguration<GymServiceClass>
+    {
+        public void Configure(EntityTypeBuilder<GymServiceClass> builder)
+        {
+            builder.HasKey(sc => new { sc.ServiceId, sc.ClassId });
+
+            
+            builder.HasOne(sc => sc.GymService)
+                .WithMany(s => s.GymServiceClasses)
+                .HasForeignKey(sc => sc.ServiceId);
+
+            
+            builder.HasOne(sc => sc.Class)
+                .WithMany(c => c.GymServiceClasses)
+                .HasForeignKey(sc => sc.ClassId);
+        }
+    }
+}
