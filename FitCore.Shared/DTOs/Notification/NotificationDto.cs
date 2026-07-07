d2c4ffd35b318985f655abb3ea80c079274aebc0
rewannmohamed <rewankhalil2004@gmail.com>
2026-07-07 16:56:39 +0300

    .

--- FitCore.Shared/DTOs/Notification/NotificationDto.cs
+++ FitCore.Shared/DTOs/Notification/NotificationDto.cs
@@ -1,18 +1,13 @@
 ﻿using FitCore.Shared.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FitCore.Shared.DTOs.Notification
 {
    public class NotificationDto
-    {
+   {
         public bool IsRead { get; set; }
         public DateTime CreatedAt { get; set; }
         public string Title { get; set; }
         public string Message { get; set; }
         public NotificationTypeEnum Type { get; set; }
-    }
+   }
 }
